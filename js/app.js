98830865bc11c38e7720b3d46a6570d1e1de301e
Abhinav Tiwari <abhinav2506293@gmail.com>
2017-04-29 16:45:36 +0530

    JS function for dynamic data entry and remove old instances of graph

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-var canvas, ctx1,ctx2,ctx3,ctx4, W, H,chartInstance,myHistogram,myDoughnutChart,myPieChart,myLineChart;
+var canvas, ctx1,ctx2,ctx3,ctx4, W, H,myHistogram,myDoughnutChart,myPieChart,myLineChart,xVal,sub;
 var labels=new Array();
 var values=new Array();
 var colors=new Array();
-var count=0,flag=0;
+var count=0;
 var grd;
 
 function init() {
@@ -35,45 +35,34 @@ function init() {
   labels = [1,2,3,4,5];
   colors = ["red","blue","purple","green","yellow"];
   values = [10,50,5,25,35];
-  flag=1;
-  pushVal();
 
+  firstTime();
 }
 
+
+/*Reset values when new Graph is to be created*/
 function fillLabel(){
-
-  labels = [];
-  values = [];
-  colors = [];
-
   myHistogram.destroy();
   myPieChart.destroy();
   myDoughnutChart.destroy();
   myLineChart.destroy();
 
-  myHistogram = new Chart(ctx1,{options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                } });
-  myPieChart = new Chart(ctx2,{options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                } });
-  myDoughnutChart = new Chart(ctx3,{options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                } });
-  myLineChart = new Chart(ctx4,{options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                } }); 
+  labels = [];
+  values = [];
+  colors = []; 
+  count=0;
 
-  var sub = document.querySelector('#num').value;
+  sub=document.querySelector("#num").value;
+  
   for(var i=1;i<=sub;i++){
     labels.push(i);
+    values.push(i);
     colors.push(getRandomColor());
-    }
-}
+  }
+
+  /* For creating an initial example containing entered number of values*/
+  firstTime();
+  }
 
 //For Generating Random Colors
 function getRandomColor() {
@@ -85,131 +74,176 @@ function getRandomColor() {
     return color;
 }
 
-function pushVal(){
-  
-  if( (count++ > document.querySelector('#num').value) && flag==0)
-  alert("Max number of data already present");
+/* For Dynamic Value update */
+function addData(){
 
-  else{
-  flag=0;  
-  var xVal = (Number)(document.getElementById('y').value);
-  values.push(xVal);
-
-  // For Histogram
-  myHistogram = new Chart(ctx1, {
-    type: 'bar',
-
-    data : {
-    labels:labels,  // X-Axis
-    datasets: [
-        {
-            label: "Histogram",
-            fill: true,
-            backgroundColor: grd,
-            hoverBackgroundColor: grd,
-            pointBackgroundColor: "#fff",
-            highlightColor:grd,
-           
-            data: values   // Y-Axis
-        }
-      ]
-    },      
-
-    options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                }                        
-});
-
-
-  // For Pie Chart
-  myPieChart = new Chart(ctx2,{
-    type: 'pie',
-    data: {
-    labels: labels,
-    datasets: [
-        {
-            label: "Pie Chart",
-            data: values,
-            backgroundColor: colors,
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-            ]
-        }]
-    },
-
-    options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                } 
-
-});
-
-  //For Doughnut Chart
-  myDoughnutChart = new Chart(ctx3, {
-    type: 'doughnut',
-    data: {
-    labels: labels,
-    datasets: [
-        {
-            label: "Doughnut Chart",
-            data: values,
-            backgroundColor: colors,
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-            ]
-        }]
-    },
-
-    options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                }
-
-});
-
-
-  //For Line Chart
-  myLineChart = Chart.Line(ctx4, {
-    type: 'bar',
-
-    data : {
-    labels:labels,  // X-Axis
-    datasets: [
-        {
-            label: "Line Chart",
-            fill: false,
-            lineTension: 0.1,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            spanGaps: false,
-            borderColor: "rgba(75,192,192,1)",
-            backgroundColor: grd,
-            pointHoverBackgroundColor: 'blue',
-            pointBackgroundColor: "#fffff",
-            highlightColor:grd,
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-            ],
-            data: values   // Y-Axis
-        }
-    ]
-    },
-
-    options: {
-                    responsive: false,
-                    maintainAspectRatio: true
-                }
-
-});
-  
-
+  myHistogram.update();
+  myPieChart.update();
+  myDoughnutChart.update();
+  myLineChart.update();
 }
+
+
+/*For pushing new data into the graph */
+function pushVal() {
+  count++;
   
+    if(count==1){
+
+    labels = [];
+    values = [];
+    colors = [];  
+
+    myHistogram.destroy();
+    myPieChart.destroy();
+    myDoughnutChart.destroy();
+    myLineChart.destroy();  
+      
+    xVal = (Number)(document.getElementById('y').value);
+    values.push(xVal);
+
+    labels.push(count);
+    colors.push(getRandomColor());
+
+    firstTime();
+    }
+
+    else if( (count > document.querySelector('#num').value))
+    alert("Max number of data already present");
+
+    else{
+
+    xVal = (Number)(document.getElementById('y').value);
+    values.push(xVal);
+
+    labels.push(count);
+    colors.push(getRandomColor());
+
+    addData();
+  }
+  
+}
+
+
+/*For the first time when the graph is created*/
+function firstTime(){
+
+  Chart.defaults.global.responsive = false;
+  Chart.defaults.global.maintainAspectRatio = true;
+  
+
+    // For Histogram
+          myHistogram = new Chart(ctx1, {
+            type: 'bar',
+
+            data : {
+            labels:labels,  // X-Axis
+            datasets: [
+                {
+                    label: "Histogram",
+                    fill: true,
+                    backgroundColor: grd,
+                    hoverBackgroundColor: grd,
+                    pointBackgroundColor: "#fff",
+                    highlightColor:grd,
+                   
+                    data: values   // Y-Axis
+                }
+              ]
+            },      
+
+            options: {
+                            responsive: false,
+                            maintainAspectRatio: true
+                        }                        
+        });
+
+
+        // For Pie Chart
+        myPieChart = new Chart(ctx2,{
+          type: 'pie',
+          data: {
+          labels: labels,
+          datasets: [
+              {
+                  label: "Pie Chart",
+                  data: values,
+                  backgroundColor: colors,
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ]
+              }]
+          },
+
+          options: {
+                          responsive: false,
+                          maintainAspectRatio: true
+                      } 
+
+      });
+
+
+        //For Doughnut Chart
+        myDoughnutChart = new Chart(ctx3, {
+          type: 'doughnut',
+          data: {
+          labels: labels,
+          datasets: [
+              {
+                  label: "Doughnut Chart",
+                  data: values,
+                  backgroundColor: colors,
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ]
+              }]
+          },
+
+          options: {
+                          responsive: false,
+                          maintainAspectRatio: true
+                      }
+
+      });
+
+
+        //For Line Chart
+      myLineChart = Chart.Line(ctx4, {
+          type: 'bar',
+
+          data : {
+          labels:labels,  // X-Axis
+          datasets: [
+              {
+                  label: "Line Chart",
+                  fill: false,
+                  lineTension: 0.1,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  spanGaps: false,
+                  borderColor: "rgba(75,192,192,1)",
+                  backgroundColor: grd,
+                  pointHoverBackgroundColor: 'blue',
+                  pointBackgroundColor: "#fffff",
+                  highlightColor:grd,
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ],
+                  data: values   // Y-Axis
+              }
+          ]
+          },
+
+          options: {
+                          responsive: false,
+                          maintainAspectRatio: true
+                    }
+
+      });
 }
